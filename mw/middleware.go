0f8538175ef17557b578deb621b70b61cb180af0
squiidz <piinkyz05@gmail.com>
2015-01-13 22:53:45 -0500

    minor changes

--- mw/middleware.go
+++ mw/middleware.go
@@ -8,6 +8,8 @@
 package mw
 
 import (
+	"compress/gzip"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -70,5 +72,30 @@ func Recovery(next http.Handler) http.Handler {
 			}
 		}()
 		next.ServeHTTP(rw, req)
+	})
+}
+
+type zipResponse struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (z zipResponse) Write(b []byte) (int, error) {
+	if z.Header().Get("Content-Type") == "" {
+		z.Header().Set("Content-Type", http.DetectContentType(b))
+	}
+	return z.Writer.Write(b)
+}
+
+// Compressing Middleware
+func Zipper(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Header().Set("Content-Encoding", "gzip")
+
+		crw := gzip.NewWriter(rw)
+		defer crw.Close()
+
+		zrw := zipResponse{Writer: crw, ResponseWriter: rw}
+		next.ServeHTTP(zrw, req)
 	})
 }
